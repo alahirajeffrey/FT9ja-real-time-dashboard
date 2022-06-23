4fe55e638de3668e7e5422d8a1a410ec93857d8b
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-22 18:10:34 -1200

    added components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = ({title}) =>{
+    return (
+        <div className="footer">
+            <h3 className="footer-title">{title}</h3>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Table.jsx
@@ -0,0 +1,30 @@
+const Table = ({ list, colNames}) => {
+    return ( 
+        <div>
+            {list.length > 0 && (
+                <table className="table" cellSpacing='0'>
+                    <thead>
+                        <tr>
+                            {colNames.map((headerItem, index)=>{
+                                <th key={index} >
+                                    {headerItem.toUpperCase()}
+                                </th>
+                            })}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Object.values(list).map((obj, index)=>{
+                            <tr key={index}>
+                                {Object.values(obj).map((value, index2)=>{
+                                    <td key={index2}>(value)</td>
+                                })}
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
+            )}
+        </div>
+     )
+}
+
+export default Table;
